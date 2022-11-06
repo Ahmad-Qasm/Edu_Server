824e0b7a346064c8c04cff05c5161e3147b8613f
ahmad qassim <amqsm.93@gmail.com>
2022-11-06 20:23:49 +0100

    The final implementation.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express')
+const express = require("express");
 const app = express();
+const { EventEmitter } = require("events");
+const Emitter = new EventEmitter();
 
-app.post('/client/:id', (req, res)=>{
-    
-})
+const waiting_clients = [];
 
-app.post('/webhook/:id', (req, res)=>{
+app.post("/client/:id", (req, res) => {
+  waiting_clients.push(req.params.id);
+  Emitter.on("proccessed", (id) => {
+    if (id === req.params.id) return res.send(id);
+  });
+});
 
-})
+app.post("/webhook/:id", (req, res) => {
+  if (waiting_clients.includes(req.params.id)) {
+    Emitter.emit("proccessed", req.params.id);
+    return res.send({ result: "ok" });
+  } else {
+    return res.send(`No client with id: ${req.params.id} could be found.`);
+  }
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
